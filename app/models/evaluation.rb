a004b73c2f9b70847289a841c1909d27cffe5dfa
Peter Kananen <pkananen@gmail.com>
2013-01-19 07:57:42 -0500

    evaluation accepts criteria

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,5 +1,6 @@
 class Evaluation < ActiveRecord::Base
   has_one :criteria
   belongs_to :session
-  attr_accessible :email, :location
+  attr_accessible :email, :location, :criteria
+  accepts_nested_attributes_for :criteria
 end
